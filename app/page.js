6ef22011979d82530d3cba67be9e93246e58b562
Alfred Okwiri <okwirialfred@gmail.com>
2023-05-27 22:03:02 +0300

    fifh commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client'
 
-import Signin from './signin/page'
+import Signin from './Signin/Page'
 import { useState } from 'react';
-import Projects from './projects/page'
+import Projects from './Projects/Page'
 export default function Home() {
   const [user, setuser] = useState(false);
   const useradd = (add) =>{
