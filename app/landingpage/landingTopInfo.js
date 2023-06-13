1d92a230f0feb255328af440248d74829dc4cbe5
Alfred Okwiri <okwirialfred@gmail.com>
2023-06-13 12:35:27 +0300

    landingpage

--- app/landingpage/landingTopInfo.js
+++ app/landingpage/landingTopInfo.js
@@ -5,7 +5,7 @@ import './landingpage.css'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
-function landingTopInfo() {
+function LandingTopInfo() {
   const router = useRouter();
   return (
     <div className='card'>
@@ -31,4 +31,4 @@ function landingTopInfo() {
   )
 }
 
-export default landingTopInfo
+export default LandingTopInfo
